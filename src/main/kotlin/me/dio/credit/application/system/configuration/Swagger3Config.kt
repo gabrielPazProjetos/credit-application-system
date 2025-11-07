838cb91d6f398a139fcf681b3a8e98b1789a789d
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-06 23:02:45 -0300

    Update Swagger3Config.kt

--- src/main/kotlin/me/dio/credit/application/system/configuration/Swagger3Config.kt
+++ src/main/kotlin/me/dio/credit/application/system/configuration/Swagger3Config.kt
@@ -1,16 +1,30 @@
 package me.dio.credit.application.system.configuration
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class Swagger3Config {
-  @Bean
-  fun publicApi(): GroupedOpenApi? {
-    return GroupedOpenApi.builder()
-      .group("springcreditapplicationsystem-public")
-      .pathsToMatch("/api/customers/**", "/api/credits/**")
-      .build()
-  }
+
+    @Bean
+    fun publicApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("springcreditapplicationsystem-public")
+            .pathsToMatch("/api/customers/**", "/api/credits/**")
+            .build()
+    }
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Credit Application System API")
+                    .version("v1")
+                    .description("Documentação da API de crédito desenvolvida com Kotlin e Spring Boot")
+            )
+    }
 }
