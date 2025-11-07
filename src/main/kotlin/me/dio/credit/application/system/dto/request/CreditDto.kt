7342e9f9e2c6acc862d94559f6ca9263071b7591
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-06 23:59:24 -0300

    Update CreditDto.kt

--- src/main/kotlin/me/dio/credit/application/system/dto/request/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/request/CreditDto.kt
@@ -10,16 +10,23 @@ import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
-  @field:NotNull(message = "Invalid input") val creditValue: BigDecimal,
-  @field:Future val dayFirstOfInstallment: LocalDate,
-  @field:Min(value = 1) @field:Max(value = 48) val numberOfInstallments: Int,
-  @field:NotNull(message = "Invalid input") val customerId: Long
-) {
+    @field:NotNull(message = "Invalid input")
+    val creditValue: BigDecimal,
 
-  fun toEntity(): Credit = Credit(
-    creditValue = this.creditValue,
-    dayFirstInstallment = this.dayFirstOfInstallment,
-    numberOfInstallments = this.numberOfInstallments,
-    customer = Customer(id = this.customerId)
-  )
+    @field:Future(message = "Installment date must be in the future")
+    val dayFirstInstallment: LocalDate,
+
+    @field:Min(value = 1, message = "Minimum 1 installment")
+    @field:Max(value = 48, message = "Maximum 48 installments")
+    val numberOfInstallments: Int,
+
+    @field:NotNull(message = "Invalid input")
+    val customerId: Long
+) {
+    fun toEntity(): Credit = Credit(
+        creditValue = this.creditValue,
+        dayFirstInstallment = this.dayFirstInstallment,
+        numberOfInstallments = this.numberOfInstallments,
+        customer = Customer(id = this.customerId)
+    )
 }
